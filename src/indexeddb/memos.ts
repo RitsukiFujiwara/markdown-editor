94d9cd05d51f03308d5b8cca27f00b00f14da1c3
Ritsuki <r9.20important@icloud.com>
2021-04-04 12:50:21 +0900

    4-3終了時

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -14,3 +14,10 @@ export const putMemo = async (title: string, text: string): Promise<void> => {
     const datetime = new Date().toISOString()
     await memos.put({ datetime, title, text })
   }
+
+  // 戻り値が配列のためMemoRecordの末尾に[]
+  export const getMemos = (): Promise<MemoRecord[]> => {
+    return memos.orderBy('datetime')
+                .reverse()
+                .toArray()
+  }
