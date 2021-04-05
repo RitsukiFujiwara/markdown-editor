cf141b544a0d15c7353260b64abe958321e42292
Ritsuki <r9.20important@icloud.com>
2021-04-05 20:31:50 +0900

    4章終了時点

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -14,10 +14,21 @@ export const putMemo = async (title: string, text: string): Promise<void> => {
     const datetime = new Date().toISOString()
     await memos.put({ datetime, title, text })
   }
+//1ページあたり10件と定義
+  const NUM_PER_PAGE: number = 10
+// memosテーブルから総件数を取得
+  export const getMemoPageCount = async (): Promise<number> => {
+    const totalCount = await memos.count()
+    const pageCount = Math.ceil(totalCount / NUM_PER_PAGE)
+    return pageCount > 0 ? pageCount : 1
+  }
 
   // 戻り値が配列のためMemoRecordの末尾に[]
-  export const getMemos = (): Promise<MemoRecord[]> => {
+  export const getMemos = (page: number): Promise<MemoRecord[]> => {
+    const offset = (page - 1) * NUM_PER_PAGE
     return memos.orderBy('datetime')
                 .reverse()
+                .offset(offset)
+                .limit(NUM_PER_PAGE)
                 .toArray()
   }
